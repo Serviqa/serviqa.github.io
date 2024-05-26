ef3e5850189071d49f9c74a18c1b1b52788a7a3c
Gideon Selorm Ayivi <selormayivi5@gmail.com>
2024-05-26 23:42:45 +0000

    feat: create hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
 import NavBar from "./components/navBar";
+import ball from "./assets/ball.svg";
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <header className="App-header"></header>
+      <header className="App-header">
+        <NavBar />
+        <div className="message">
+          <h2> STAY TUNED</h2>
+          <p> We are working on something awesome! </p>
+
+          <img src={ball} className="ball" alt="ball" />
+        </div>
+      </header>
     </div>
   );
 }
