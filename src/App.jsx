706570a34de5a53ea1e9b7fd59bd767a4798eeb3
Gideon Selorm Ayivi <selormayivi5@gmail.com>
2024-05-28 15:53:06 +0000

    feat: create footer section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import "./App.css";
 import NavBar from "./components/navBar";
 import ball from "./assets/ball.svg";
 import comingSoon from "./assets/coming-soon.svg";
+import brand from "./assets/brand.svg";
+import phone from "./assets/phone.svg";
+import email from "./assets/email.svg";
+import location from "./assets/location.svg";
 
 function Header() {
   return (
@@ -28,7 +32,63 @@ function Main() {
 function Footer() {
   return (
     <footer>
-      <p> © 2021, All rights reserved.</p>
+      <div className="brand-container">
+        <img src={brand} className="brand" alt="brand" />
+      </div>
+      <hr />
+      <div className="footer-links">
+        <div className="address">
+          <h3>Reach Us</h3>
+          <p>
+            <img src={phone} alt="phone icon" /> 123-456-7890
+          </p>
+          <p>
+            <img src={email} alt="email icon" /> info@sel-labs.com
+          </p>
+          <p>
+            <img src={location} alt="location icon" />
+            Dansoman Accra Ghana
+          </p>
+        </div>
+        <div className="links">
+          <div className="company">
+            <h3>Company</h3>
+            <ul>
+              <li>Portfolio</li>
+              <li>Services</li>
+              <li>Careers</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+          <div className="legal">
+            <h3>Legal</h3>
+            <ul>
+              <li>Privacy Policy</li>
+              <li>Terms &amp; Services</li>
+              <li>Refund Policy</li>
+            </ul>
+          </div>
+          <div className="quick-links">
+            <h3>Quick Links</h3>
+            <ul>
+              <li>Blog</li>
+              <li>FAQ</li>
+              <li>Support</li>
+            </ul>
+          </div>
+        </div>
+        <div className="newsletter">
+          <h3>Join Our Newsletter</h3>
+          <p>
+            You&apos;ll receive updates on our latest discout and offers
+            directly in your inbox.
+          </p>
+          <form action="">
+            <input type="email" placeholder="Enter your email" />
+            <button>Subscribe</button>
+          </form>
+        </div>
+      </div>
     </footer>
   );
 }
