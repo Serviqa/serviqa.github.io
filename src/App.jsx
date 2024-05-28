6178e8ddc13426ae73854b935a6c4103f61707e7
Gideon Selorm Ayivi <selormayivi5@gmail.com>
2024-05-28 13:45:47 +0000

    feat: create main section

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,44 @@
 import "./App.css";
 import NavBar from "./components/navBar";
 import ball from "./assets/ball.svg";
+import comingSoon from "./assets/coming-soon.svg";
 
-function App() {
+function Header() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <NavBar />
-        <div className="message">
-          <h2> STAY TUNED</h2>
-          <p> We are working on something awesome! </p>
+    <header className="App-header">
+      <NavBar />
+      <div className="message">
+        <h2> STAY TUNED</h2>
+        <p> We are working on something awesome! </p>
 
-          <img src={ball} className="ball" alt="ball" />
-        </div>
-      </header>
-    </div>
+        <img src={ball} className="ball" alt="ball" />
+      </div>
+    </header>
   );
 }
 
-export default App;
+function Main() {
+  return (
+    <main>
+      <img src={comingSoon} className="coming-soon" alt="coming-soon" />
+    </main>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p> © 2021, All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
