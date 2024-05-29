16ee9d430611cc3522710dde2b97c94cd2c139c3
Gideon Selorm Ayivi <selormayivi5@gmail.com>
2024-05-29 03:08:32 +0000

    feat: create mobile screen version

--- src/App.jsx
+++ src/App.jsx
@@ -16,18 +16,19 @@ function Header() {
         <p> We are working on something awesome! </p>
 
         <img src={ball} className="ball" alt="ball" />
+        <img src={comingSoon} className="coming-soon" alt="coming-soon" />
       </div>
     </header>
   );
 }
 
-function Main() {
-  return (
-    <main>
-      <img src={comingSoon} className="coming-soon" alt="coming-soon" />
-    </main>
-  );
-}
+// function Main() {
+//   return (
+//     <main>
+//       <img src={comingSoon} className="coming-soon" alt="coming-soon" />
+//     </main>
+//   );
+// }
 
 function Footer() {
   return (
@@ -39,41 +40,61 @@ function Footer() {
       <div className="footer-links">
         <div className="address">
           <h3>Reach Us</h3>
-          <p>
-            <img src={phone} alt="phone icon" /> 123-456-7890
-          </p>
-          <p>
+          <a href="tel:+233592318393">
+            <img src={phone} alt="phone icon" /> 059-231-8393
+          </a>
+          <a href="mailto:selormayivi5@gmail.com">
             <img src={email} alt="email icon" /> info@sel-labs.com
-          </p>
-          <p>
+          </a>
+          <a>
             <img src={location} alt="location icon" />
-            Dansoman Accra Ghana
-          </p>
+            Dansoman - Accra
+          </a>
         </div>
         <div className="links">
           <div className="company">
             <h3>Company</h3>
             <ul>
-              <li>Portfolio</li>
-              <li>Services</li>
-              <li>Careers</li>
-              <li>Contact</li>
+              <li>
+                <a href="">Portfolio</a>
+              </li>
+              <li>
+                <a href="">Services</a>
+              </li>
+              <li>
+                <a href="">Careers</a>
+              </li>
+              <li>
+                <a href="">Contact</a>
+              </li>
             </ul>
           </div>
           <div className="legal">
             <h3>Legal</h3>
             <ul>
-              <li>Privacy Policy</li>
-              <li>Terms &amp; Services</li>
-              <li>Refund Policy</li>
+              <li>
+                <a href="">Privacy Policy</a>
+              </li>
+              <li>
+                <a href="">Terms &amp; Services</a>
+              </li>
+              <li>
+                <a href="">Refund Policy</a>
+              </li>
             </ul>
           </div>
           <div className="quick-links">
             <h3>Quick Links</h3>
             <ul>
-              <li>Blog</li>
-              <li>FAQ</li>
-              <li>Support</li>
+              <li>
+                <a href="">Blog</a>
+              </li>
+              <li>
+                <a href="">FAQ</a>
+              </li>
+              <li>
+                <a href="">Support</a>
+              </li>
             </ul>
           </div>
         </div>
@@ -97,7 +118,7 @@ export default function App() {
   return (
     <div className="App">
       <Header />
-      <Main />
+      {/* <Main /> */}
       <Footer />
     </div>
   );
